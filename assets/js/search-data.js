3ca7230d8b994facf402e85f15cb30744144e23a
sayedhmumit <172433431+sayedhmumit@users.noreply.github.com>
2026-06-09 20:01:13 +0000

    Deploying to gh-pages from @ sayedhmumit/sayedhmumit.github.io@884ebc3b19d3bc276e32cedbaf146bd9f8759dc6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,9 +409,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-gravitational-spacetime-in-triangular-merge",
+          title: 'Gravitational Spacetime in Triangular Merge',
+          description: "Discrete geometry, graviton charges, and simplicial decomposition of curved spacetime",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
