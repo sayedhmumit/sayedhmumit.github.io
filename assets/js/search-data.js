fd13b7ead9238fc6ab009fe5b41c26a4847706f5
sayedhmumit <172433431+sayedhmumit@users.noreply.github.com>
2026-06-09 20:04:36 +0000

    Deploying to gh-pages from @ sayedhmumit/sayedhmumit.github.io@0ce2180c6b00a3a8af2601a8691c9f44fb993601 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,7 +497,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.facebook.com/Personal.Mumit", "_blank");
+          window.open("https://github.com/sayedhmumit/", "_blank");
         },
       },{
       id: 'light-theme',
